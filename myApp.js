17fa2e81a39ec70e6980b56e35898affaf0ae5f6
+ <andrewjohnjulian@gmail.com>
2023-03-31 22:10:22 -0600

    work on node introduction day #1

--- myApp.js
+++ myApp.js
@@ -1,39 +1,27 @@
-let express = require('express');
+let express = require("express");
+require("dotenv").config();
 let app = express();
 
+console.log("Hello World");
 
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+/* app.get("/", (req, res) => {
+  res.send("Hello Express");
+}); */
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else res.json({ message: "Hello json" });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
