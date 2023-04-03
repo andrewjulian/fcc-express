30fbe5a3f71142b96069a4eab459cb5b159e7237
+ <andrewjohnjulian@gmail.com>
2023-04-02 21:40:30 -0600

    Working with app requests practice

--- myApp.js
+++ myApp.js
@@ -24,4 +24,15 @@ app.get("/json", (req, res) => {
   } else res.json({ message: "Hello json" });
 });
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
+
 module.exports = app;
